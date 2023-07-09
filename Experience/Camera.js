8f585ba0cc4dcf1483640d72348ba1933df4d13a
Chaithu Talasila <75641428+themoddedcube@users.noreply.github.com>
2023-07-09 18:45:57 -0500

    added preloader fix

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -64,11 +64,9 @@ export default class Camera {
     }
 
     resize() {
-        // Updating Perspective Camera on Resize
         this.perspectiveCamera.aspect = this.sizes.aspect;
         this.perspectiveCamera.updateProjectionMatrix();
 
-        // Updating Orthographic Camera on Resize
         this.orthographicCamera.left =
             (-this.sizes.aspect * this.sizes.frustrum) / 2;
         this.orthographicCamera.right =
